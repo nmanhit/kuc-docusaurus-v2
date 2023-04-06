458ca4e43a1f25439e1755c97e6a84efd50c12bc
Nguyen Hoang Anh <nmanhnit@gmail.com>
2023-04-06 15:56:39 +0700

    update jp language on home page

--- docs/document/src/pages/index.js
+++ docs/document/src/pages/index.js
@@ -7,23 +7,49 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+function getCurrentLanguage() {
+  const {siteConfig} = useDocusaurusContext();
+  const baseUrl = siteConfig.baseUrl;
+  if(baseUrl.includes("/ja/")) return "ja";
+  return "en";
+}
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/quick-start">
-            Getting Started
-          </Link>
+  const currentLanguage = getCurrentLanguage();
+  if(currentLanguage === "en") {
+    return (
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/getting-started/quick-start">
+              Getting Started
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">Be a smart kintone developer with kintone UI Component.</h1>
+          <p className="hero__subtitle">{siteConfig.taglineJP}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/getting-started/quick-start">
+              Getting Started
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default function Home() {
